cb900886160516adbad0cf9d56755c13110c153b
GHOST <ghost_man01@kali>
2021-10-16 14:44:27 -0400

    Adding input_output.py

---
+++ input_output.py
@@ -0,0 +1,7 @@
+#!/bin/env python
+
+user_input = input( "What's your name : " );        # Remind semi-colon after paranthesis
+print( user_input );                # No string when it's integer
+
+user_input1 = raw_input( "What's your name : " ); # raw_input needs a plus sign 
+print( "Hi\n" + user_input1 )
